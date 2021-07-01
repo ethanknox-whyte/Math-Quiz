d44aa858c4bac83a511ec632c4a910869a3ec427
Ethan Knox-Whyte <EKnox-Whyte@stu.kerikerihigh.ac.nz>
2021-07-02 11:57:54 +1200

    First version

---
+++ skateQuiz.py
@@ -0,0 +1,21 @@
+import keyboard
+
+print("\nWelcome to the Best Skateboard Quiz. \n\nPress < ENTER > to play. \nPress < BACKSPACE > to quit\n ")
+
+question1 = {'question':"Who was the star of Pro Skater 2?", "correct ans": "Tony Hawk", "wrong ans": ['Bob Burnquist', 'Rune Glifberg', 'Kareem Campbell']}
+question2 = {'question':"What was the biggest stair set ever ollied?", "correct ans": "Lyon 25", "wrong ans": ['El Toro', 'Hollywood High', 'UC Davis']}
+question3 = {'question':"Who ollied the biggest stair set?", "correct ans": "Aaron Homoki", "wrong ans": ['Nyjah Huston', 'Tony Hawk', 'Andrew Reynolds']}
+question4 = {'question':"Who was the Skate School Teacher in Skate 3?", "correct ans": "Coach Frank", "wrong ans": ['Coach Fred', 'Coach Aaron', 'Coach Franklin']}
+question5 = {'question':"Who was the 2021 Worldwide Skateboarding Champion?", "correct ans": "Yuto Horigome", "wrong ans": ['Nyjah Huston', 'Aaron Kyro', 'Reef Owen']}
+question6 = {'question':"Who is the best skateboarder in New Zealand?", "correct ans": "Bowman Hansen", "wrong ans": ['Izy Mutu', 'Shaun Boucher', 'Theo Clarke']}
+question7 = {'question':"Where was the 2019 X Games held?", "correct ans": "Minneaplois", "wrong ans": ['Los Angeles', 'Miami', 'Huston']}
+question8 = {'question':"What was skateboarding originally called?", "correct ans": "Sidewalk Surfing", "wrong ans": ['Flat Boarding', 'Street Waving', 'Street Surfing']}
+question9 = {'question':"How many skateboard decks are made each month", "correct ans": "500,000", "wrong ans": ['100,000', '350,000', '1,000,000']}
+question10 = {'question':"What part of the body is injured in skateboarding?", "correct ans": "Wrists", "wrong ans": ['Leg', 'Foot', 'Arm']}
+
+
+print(question1['wrong ans'][0])
+
+
+
+    
